59a7e591dfcb94e54af2a6cd997b4969d49bf9e2
VerdiZone <verdizone@gmail.com>
2021-12-26 14:48:05 -0600

    primer commit proyecto con sass

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import './HomePage.scss'
+
+class HomePage extends React.Component {
+    render() { 
+        return <div>
+        <div className='homepage'>
+          <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        TROMBÓN
+                        <span className='subtitle'>
+                            COMPRAR AHORA
+                        </span>
+                    </h1>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        TROMBÓN
+                        <span className='subtitle'>
+                            COMPRAR AHORA
+                        </span>
+                    </h1>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        TROMBÓN
+                        <span className='subtitle'>
+                            COMPRAR AHORA
+                        </span>
+                    </h1>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        TROMBÓN
+                        <span className='subtitle'>
+                            COMPRAR AHORA
+                        </span>
+                    </h1>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">
+                        TROMBÓN
+                        <span className='subtitle'>
+                            COMPRAR AHORA
+                        </span>
+                    </h1>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        </div>;
+    }
+}
+ 
+export default HomePage;
